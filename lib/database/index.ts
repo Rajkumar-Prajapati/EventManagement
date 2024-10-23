410f3b87f458608ff3f9207a21910709c4e6b81f
Rajkumar Prajapati <120646423+Rajkumar-Prajapati@users.noreply.github.com>
2024-10-23 15:44:10 +0530

    Update index.ts

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,34 +1,21 @@
-import mongoose, { Connection } from 'mongoose';
+
+import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-interface CachedMongoose {
-  conn: Connection | null;
-  promise: Promise<Connection> | null;
-}
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
-// Avoid using `var` by directly checking for global.mongoose
-let cached: CachedMongoose = global.mongoose || { conn: null, promise: null };
-
-export const connectToDatabase = async (): Promise<Connection> => {
-  // Return cached connection if it exists
+export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  // Throw an error if MONGODB_URI is missing
-  if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-  // If no connection, create a new promise for connection
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      dbName: 'EventManagement',
-      bufferCommands: false,
-    });
-  }
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently',
+    bufferCommands: false,
+  })
 
   cached.conn = await cached.promise;
-  
-  // Safely store the connection globally using `let`
-  global.mongoose = cached;
 
   return cached.conn;
-};
+}
