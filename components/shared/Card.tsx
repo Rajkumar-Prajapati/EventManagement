da34c7403fd03c631c72eaaf962ecd2575cc5fd8
Rajkumar Prajapati <120646423+Rajkumar-Prajapati@users.noreply.github.com>
2024-10-23 13:31:42 +0530

    Update Card.tsx

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -5,7 +5,7 @@ import { auth } from '@clerk/nextjs/server'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import  DeleteConfirmation from './DeleteConfirmation';
+import { DeleteConfirmation } from './DeleteConfirmation'
 
 type CardProps = {
   event: IEvent,
